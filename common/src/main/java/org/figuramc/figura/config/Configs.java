c452358d2a4896d8166c0c022e7c7f627db82269
红血球AE3803 <2544390577@qq.com>
2026-03-23 21:35:01 +0900

    Update PAPERDOLL_OFFSET values in Configs.java

--- common/src/main/java/org/figuramc/figura/config/Configs.java
+++ common/src/main/java/org/figuramc/figura/config/Configs.java
@@ -197,8 +197,8 @@ public class Configs {
             PAPERDOLL_SCALE = new ConfigType.FloatConfig("paperdoll_scale", PAPERDOLL, 1f),
             PAPERDOLL_X = new ConfigType.FloatConfig("paperdoll_x", PAPERDOLL, 0f),
             PAPERDOLL_Y = new ConfigType.FloatConfig("paperdoll_y", PAPERDOLL, 0f),
-            PAPERDOLL_OFFSET_X = new ConfigType.FloatConfig("paperdoll_offset_x", PAPERDOLL, 80),
-            PAPERDOLL_OFFSET_Y = new ConfigType.FloatConfig("paperdoll_offset_y", PAPERDOLL, 65),
+            PAPERDOLL_OFFSET_X = new ConfigType.FloatConfig("paperdoll_offset_x", PAPERDOLL, 380),
+            PAPERDOLL_OFFSET_Y = new ConfigType.FloatConfig("paperdoll_offset_y", PAPERDOLL, 150),
             PAPERDOLL_PITCH = new ConfigType.FloatConfig("paperdoll_pitch", PAPERDOLL, 0f),
             PAPERDOLL_YAW = new ConfigType.FloatConfig("paperdoll_yaw", PAPERDOLL, 20);
 
